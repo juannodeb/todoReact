b8506ae3fe1b8ac2913eecaa64eb96aff84ed006
juannodeb <juannodeb@gmail.com>
2020-06-11 19:21:32 -0500

    Created components and containers folders, Task and TaskCOncntainer components and linked them with App.js

---
+++ src/containers/TaskContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Task from '../components/Task';
+
+const TaskContainer = () => {
+  return (
+    <div>
+      <Task />
+    </div>
+  )
+}
+
+export default TaskContainer;
